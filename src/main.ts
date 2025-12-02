651377c9b9114ea52c58b4f3f7ed0d5d553961e3
Azizbek Bariyev <xashimudfarrux@gmail.com>
2025-12-02 09:54:32 +0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as express from 'express';
+import cookieParser from 'cookie-parser';
+
+async function start() {
+  try {
+    const PORT = process.env.PORT || 3030;
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    const allowedOrigins = ['https://bot.shamseducation.uz'];
+
+    app.enableCors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          console.log('❌ CORS blocked:', origin);
+          callback(new Error('CORS blocked!'), false);
+        }
+      },
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'telegram-init-data',
+        'cache-control',
+      ],
+      exposedHeaders: ['set-cookie'],
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    });
+    app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    app.use(cookieParser());
+    app.setGlobalPrefix('api');
+
+    await app.listen(PORT, () => {
+      console.log(`🚀 Server started at http://localhost:${PORT}/api`);
+    });
+  } catch (error) {
+    console.log('❌ Server start error:', error);
+  }
+}
+start();
